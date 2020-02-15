3ab6947a50f2c0b547ff29f865f80dd5cadbb44f
Thor Veen <thorveen@Thors-MacBook-Pro.local>
2020-02-14 16:59:44 -0800

    update code of today
    tuned it a bit and more comments. Should work nicely.

--- day 10 various code BDC.R
+++ day 10 various code BDC.R
@@ -44,32 +44,43 @@ for(i in 1:nrow(d)){
   }
 }
 
-d$more.data <- rnorm(nrow(d), 4, 3)
+# soem code to add an extra column and variable
+#d$more.data <- rnorm(nrow(d), 4, 3)
 
-columns.compare <- c(2,3,4,5)
+
+# code to make plots and store as pdf through a loop
+# also store output of a statsical model in an object
+
+columns.compare <- c(2,3,4,5) # columns to be compared 2 against 3, 2 -4, 2-5 
 store.t <- rep(NA, length(columns.compare)-1)
-country.names <- c("wwo","hjk","rr", "oo")
 
-?merge
+country.names <- c("plot 1","plot 2","plot 3", "plot 4") # made this up.
 
-#for(i in 1: length(columns.compare)-1 ){
+# the usual, loop through number of comparisons (length(columns.compare)-1)
+for(i in 1: length(columns.compare)-1 ){
   
-  i <- 1
+  #i <- 1
+  # regression
   model.t <- lm(d[,columns.compare[1]] ~ d[, columns.compare[i + 1] ])
-  
+  # store the model coefficent, this case the first one (= intercept)
+  store.t[i] <- model.t$coefficients[1]
+  # make a figure of two variables and save it as pdf
   pdf(paste(country.names[i], ".pdf", sep = ""))
   plot( d[,columns.compare[1]], d[, columns.compare[i + 1] ],las = 1, 
         xlab = "var.x", ylab = "var.x", col = "red", pch = 16,
         main = country.names[i])
-  dev.off()
-  store.t[i] <- model.t$coefficients[1]
-#}
+  dev.off() # don't forget this...
+ 
+}
 
 
-# substring
+# ---- regular expression
+# substring : to get a specific set of characters (start, end) from a 
+# char string.
 
 # string of characters
 t <- c("This is pretty awesome")
+# get characters from position 1 to 6
 substr(t, 1, 6)
 
 
